469af714a6a664652fdda263168d1df926463fb3
awaal <awaal@localhost>
2019-12-19 14:07:56 +0100

    Initial commit

---
+++ src/main/java/org/waal70/utils/document/Main.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package org.waal70.utils.document;
+
+import org.apache.log4j.Logger;
+
+/**
+ * @author awaal
+ *
+ */
+public class Main {
+	private static Logger log = Logger.getLogger(Main.class);
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		log.info("Hello");
+
+	}
+
+}
